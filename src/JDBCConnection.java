e58d3c31d8b41123b7c0f6992026d7fd7b4952e8
Sunil Gurung <mrgurung2553@gmail.com>
2016-05-08 13:00:50 -0400

    first add

---
+++ src/JDBCConnection.java
@@ -0,0 +1,31 @@
+
+import java.sql.*;
+
+public class JDBCConnection {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("change as been made");
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:oracle", "c##anand", "sunil");
+			Statement st =conn.createStatement();
+			String sql = "select * from jdbcconnection";
+			ResultSet re = st.executeQuery(sql);
+			while(re.next()){
+				System.out.println(re.getString(1));
+				
+			}
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			System.out.println("class not found");
+			
+		} catch (SQLException e) {
+			 e.printStackTrace();
+		}
+		
+		
+		
+
+	}
+
+}
